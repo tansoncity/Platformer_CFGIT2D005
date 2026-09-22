10f580b2e90f1b0e89226c2b743553445e88de0f
Duong Hoang Thanh <thanh1807@yahoo.com>
2026-09-22 23:28:17 +0700

    feat: Simpler & faster paralax

--- Assets/Scripts/Paralax.cs
+++ Assets/Scripts/Paralax.cs
@@ -17,30 +17,29 @@ public class Paralax : MonoBehaviour
         _width = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    private void LateUpdate() => UpdateDistance();
+    private void LateUpdate()
+    {
+        UpdateDistance();
+        ApplyDistanceLoop();
+    }
 
     private void UpdateDistance()
     {
         var cameraDistance = _targetCamera.position - _cameraStartPosition;
-        var newPosition = _startPosition;
-        newPosition.x += cameraDistance.x * _horizontalScale;
-        newPosition.y += cameraDistance.y * _verticalScale;
-
-        ApplyDistanceLoop(ref newPosition);
-        transform.position = newPosition;
+        transform.position = _startPosition + 
+            new Vector3(cameraDistance.x * _horizontalScale, cameraDistance.y * _verticalScale, 0);
     }
 
-    private void ApplyDistanceLoop(ref Vector3 newPosition)
+    private void ApplyDistanceLoop()
     {
-        var xOffset = newPosition.x - _targetCamera.position.x;
-        while (xOffset < -_width / 2)
+        var xOffset = transform.position.x - _targetCamera.position.x;
+        if (xOffset < -_width / 2)
         {
-            xOffset += _width;
+            _startPosition.x += _width;
         }
-        while (xOffset > _width / 2)
+        else if (xOffset > _width / 2)
         {
-            xOffset -= _width;
+            _startPosition.x -= _width;
         }
-        newPosition.x = _targetCamera.position.x + xOffset;
     }
 }
